bbaecef57edfc29c44d6e7ec28f60684d9f2593e
RohitSaharan0398 <rohit8607745768@gmail.com>
2025-02-24 13:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function mincost(arr)
 { 
 //write your code here
+	arr.sort();
+	sum=arr[0];
+	let arr1= new Array(0);
+	for(int i=0;i<arr.length-1;i++){
+       sum+=arr[i+1];
+		arr1.push(sum);
+	}
+	sum=0;
+	arr1.forEach((element)=>{
+		sum+=element;
+	});
+	
+	
+	return sum;
 // return the min cost
   
 }
